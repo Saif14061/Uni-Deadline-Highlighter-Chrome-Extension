009c3eb5c08d6c94b60c9b820c6cb9ba78f10a9d
N1397190 <saif.abbas0192@gmail.com>
2026-03-03 15:28:09 +0000

    working on DOM

--- logic.js
+++ logic.js
@@ -1,6 +1,16 @@
-let elements = document.querySelectorAll("p")
-
-elements.forEach(function(element){
-    console.log(element.textContent)
-
+let observer = new MutationObserver(function(){
+    scanPage()
 })
+
+observer.observe(document.body,{childList: true, subtree: true})
+function scanPage(){
+    let elements = document.querySelectorAll("p,div,span")
+    let pattern = /\d{1,2}\/\d{1,2}\/\d{2,4}/
+
+    elements.forEach(function(element){
+        let text = element.textContent
+        if(text.includes("Due")){
+            console.log("found deadline" + text)
+        }
+    })
+}
